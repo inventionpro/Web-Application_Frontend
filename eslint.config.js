4da351315788fff96db1d918693a6108be4d5c3b
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-01-20 19:27:17 +0100

    Update eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import vue from 'eslint-plugin-vue';
+
+export default [
+  ...vue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: {
+      vue
+    },
+    rules: {
+      'id-length': ['warn', { min: 0, max: 35 }],
+      'vue/max-attributes-per-line': 'off'
+    }
+  }
+];
